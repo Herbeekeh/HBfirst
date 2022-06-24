36ca2e7bff545b3796a55d843d1a19e51a7ff9f6
Herbeekeh <tijanikafilatabike@gmail.com>
2022-06-24 19:12:41 +0100

    #first

---
+++ HB.js
@@ -0,0 +1,13 @@
+var firstname = "Kafilat";
+var lastname = "Tijani";
+
+
+var fullname = firstname + " " + lastname;
+
+let height = "5ft"
+let country = "Nigeria"
+
+
+console.log(fullname)
+console.log(height)
+console.log(country)
